534419829e4346460c649741ed0f8935e6d66da1
Dario Cepele <64326626+DarioCepele@users.noreply.github.com>
2023-04-02 17:14:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function sendMail() {
+  const serviceID = "service_leqjc2h";
+  const templateID = "template_1wnedo7";
+  var mail = {
+    name: document.getElementById("email").value
+  }
+
+  emailjs.send(serviceID, templateID, mail)
+  .then(
+  res =>{
+    document.getElementById("email").value = "";
+    console.log(res);
+    alert("Your message sent successfully");
+  }
+  )
+  .catch((err) => console.log(err));
+}
+
